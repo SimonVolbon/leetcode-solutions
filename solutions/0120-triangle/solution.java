0e59e7359badcb288dccf1c394fe9dd3ab962377
Simon Volbon <simon.volbon@gmail.com>
2025-09-25 07:43:27 +0000

    [LeetCode Sync] Runtime - 9 ms (11.07%), Memory - 45.2 MB (84.47%)

---
+++ solutions/0120-triangle/solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        for (int i = triangle.size() - 2; i >= 0; i--) {
+            for (int j = 0; j < triangle.get(i).size(); j++) {
+                int minBelow = Math.min(
+                    triangle.get(i + 1).get(j),
+                    triangle.get(i + 1).get(j + 1)
+                );
+                int currentVal = triangle.get(i).get(j);
+                triangle.get(i).set(j, currentVal + minBelow);
+            }
+        }
+        return triangle.get(0).get(0);
+
+    }
+
+}
