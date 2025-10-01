e8b7096ad9eae98e12964d71bcc40059e92409bd
Simon Volbon <simon.volbon@gmail.com>
2025-10-01 12:09:15 +0000

    [LeetCode Sync] Runtime - 325 ms (5.14%), Memory - 45.6 MB (8.12%)

---
+++ solutions/0830-largest-triangle-area/solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public double largestTriangleArea(int[][] points) {
+        double biggestArea = 0;
+        for(int i = 0; i < points.length - 2; i++) {
+            for(int j = i + 1; j < points.length - 1; j++) {
+                for(int k = j + 1; k < points.length; k++) {
+                    double area = 0.5f * (double) Math.abs(points[i][0]*(points[j][1]-points[k][1]) + points[j][0]*(points[k][1]-points[i][1]) + points[k][0]*(points[i][1] - points[j][1]));
+                    System.out.println(area);
+                    if(area > biggestArea){
+                        biggestArea = area;
+                    }
+                }
+            }
+        }
+
+        return biggestArea;
+    }
+}
